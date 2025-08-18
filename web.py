861b5e367e68b435a412e4b95b710bcc50963ee7
dhanushl0l <dhanushdjtherock@gmail.com>
2025-08-19 01:49:35 +0530

    feat: added conversation history

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
+import logging
 import os
-from typing import Awaitable, Callable, Optional
+from typing import Awaitable, Callable, Iterable, List, Optional
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import FileResponse, HTMLResponse
 from openai import AsyncOpenAI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from openai.types.chat import ChatCompletionMessageParam  
 
 from config import load_config_from_json
 from main import NAME
@@ -25,43 +27,51 @@ def start_web(request: Request):
 @app.websocket("/ws/chat")
 async def websocket_chat(websocket: WebSocket):
     await websocket.accept()
+    chat: Iterable[ChatCompletionMessageParam] = []
+    chat.append(
+        {"role": "system", "content": SETTINGS.ROLE},
+    )
     try:
         async def send(val: str) -> None:
             await websocket.send_text(val)
         while True:
             data: str = await websocket.receive_text()
             await websocket.send_text("[[END]]")
-            await get_message_async(data, send) 
+            await get_message_async(data, chat, send) 
     except Exception as e:
-        print("WebSocket closed:", e)
+        logging.info("WebSocket closed:", e)
 
 @app.get("/favicon.ico")
 async def favicon():
     file_path = os.path.join("static", "assets", "theam.svg") 
     return FileResponse(file_path)
 
+
 async def get_message_async(
     prompt: str,
-    channel: Optional[Callable[[str], Awaitable[None]]] = None) -> None:
+    chat: list[ChatCompletionMessageParam],
+    channel: Optional[Callable[[str], Awaitable[None]]] = None
+    ) -> None:
     client = AsyncOpenAI(api_key=SETTINGS.API_KEY, base_url=SETTINGS.BASE_URL)
-
     async def send(msg: str):
         if channel:
             await channel(msg)
-
+    chat.append(
+         {"role": "user", "content": prompt}
+    )
     try:
         resp = await client.chat.completions.create(
             model=SETTINGS.MODEL,
-            messages=[
-                {"role": "system", "content": SETTINGS.ROLE},
-                {"role": "user", "content": prompt},
-            ],
+            messages=chat,
             temperature=SETTINGS.TEMPERATURE,
             top_p=0.9,
             frequency_penalty=0.5,
             presence_penalty=0.0,
         )
         text = (resp.choices[0].message.content or "").strip()
+        chat.append(
+        {"role": "assistant", "content": text},
+        )
         rewrite_prompt = f"{SETTINGS.REWRITE_PROMPT}\nText:\n{text}"
         stream = await client.chat.completions.create(
             model=SETTINGS.MODEL,
@@ -83,7 +93,8 @@ async def get_message_async(
                 full_message_parts.append(token)
 
         final_text = "".join(full_message_parts)
-        print(final_text)
+        logging.debug(final_text)
 
     except Exception as e:
         await send(f"[Error] {str(e)}")
+        
